f763668d5d2433115eab714b202d593353da060a
John Peck <sunnyvalejp@gmail.com>
2013-02-15 23:30:49 -0600

    Added usb board file to the howto

--- submod.sh
+++ submod.sh
@@ -11,6 +11,15 @@ USB_JPEDA=./implement/boards/usb/reva/schematics/jpeda
 # The path to the Butterfly board's jpeda module
 BFLY_JPEDA=./implement/boards/butterfly/reva/schematics/jpeda
 
+# The howto doctools
+HOWTO_DOCTOOLS=./implement/howto/doctools
+
+# --------------------- Done with configuration -------------------------
+SCRIPTDIR=$(pwd -P)
+
+
+# Check out a version of jpeda for the USB board
+cd $SCRIPTDIR
 if [ -d "$USB_JPEDA" ]; then
     # The directory already exists
     cd $USB_JPEDA;git pull
@@ -18,9 +27,21 @@ else
     git clone git@github.com:johnpeck/jpeda.git $USB_JPEDA  
 fi
 
+
+# Check out a version of jpeda for the butterfly board
+cd $SCRIPTDIR
 if [ -d "$BFLY_JPEDA" ]; then
     # The directory already exists
     cd $BFLY_JPEDA;git pull
 else
     git clone git@github.com:johnpeck/jpeda.git $BFLY_JPEDA  
+fi
+
+# Check out a version of doctools for the howto
+cd $SCRIPTDIR
+if [ -d "$HOWTO_DOCTOOLS" ]; then
+    # The directory already exists
+    cd $HOWTO_DOCTOOLS;git pull
+else
+    git clone git@github.com:johnpeck/doctools.git $HOWTO_DOCTOOLS 
 fi
