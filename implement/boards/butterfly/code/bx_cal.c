be8ee821d9667ade111b28567d917a9290b77e67
John Peck <sunnyvalejp@gmail.com>
2013-05-28 15:40:07 -0500

    Modified chassis sticker

--- implement/boards/butterfly/code/bx_cal.c
+++ implement/boards/butterfly/code/bx_cal.c
@@ -42,10 +42,30 @@
 */
 #include "bx_current.h"
 
+/* bx_functions.h
+
+   Defines system_state_t for the instrument serial number
+*/
+#include "bx_functions.h"
+
+
+
+
+
+// --------------------- EEPROM addresses ----------------------------
+
+/* Define the offset for general system variables. The eeprom has 512
+   total locations.  I'll allocate addresses 0-99 for general system
+   storage.
+*/
+/* The base address for the serial number.  The serial number is two
+   bytes wide.  The next open address is 2.
+*/
+#define SERNUM_ADDR 0
+
 
 /* Define the offset for each calibration coefficient in the array.
-   The eeprom has 512 total locations.  I'll allocate 100-200 for
-   calibration factors.
+   I'll allocate addresses 100-200 for calibration factors.
 */
 /* The base address for the current slope factor.  The slope occupies
    two locations. The next open address is 102.
@@ -61,13 +81,7 @@
 
 // ----------------------- Functions ----------------------------------
 
-/* cal_init()
 
-   Read cal elements out of eeprom
-*/
-void cal_init(void) {
-  return;
-}
 
 /* cal_save_islope( current output slope calibrationa factor )
 
@@ -96,6 +110,7 @@ void cal_save_islope(uint16_t slope){
 void cmd_write_islope( command_arg_t *command_arg_ptr ) {
   uint16_t slope = (command_arg_ptr -> uint16_arg);
   cal_save_islope(slope);
+  current_init();
 }
 
 /* cal_save_ioffset( current output offset calibration factor )
@@ -124,6 +139,7 @@ void cal_save_ioffset(int16_t offset) {
 void cmd_write_ioffset( command_arg_t *command_arg_ptr ) {
   int16_t offset = (command_arg_ptr -> sint16_arg);
   cal_save_ioffset(offset);
+  current_init();
 }
 
 
@@ -138,4 +154,39 @@ void cal_load_current( current_cal_t *current_cal ) {
   lowbyte = eeprom_read_char(I_OFFSET_ADDR);
   highbyte = eeprom_read_char(I_OFFSET_ADDR + 1);
   current_cal -> offset = (int16_t)( (highbyte << 8) + lowbyte);
+}
+
+
+/* cal_save_sernum( instrument serial number )
+ */
+void cal_save_sernum( uint16_t sernum ) {
+  logger_msg_p( "cal", log_level_INFO,
+		PSTR("Writing serial number %u\r\n"),sernum);
+  uint8_t lowbyte = (uint8_t)(sernum & 0xff);
+  uint8_t highbyte = (uint8_t)( (sernum & 0xff00) >> 8);
+  eeprom_write_char(SERNUM_ADDR,lowbyte);
+  eeprom_write_char(SERNUM_ADDR + 1, highbyte);
+}
+
+/* cal_load_sernum( pointer to system status structure )
+
+   Loads the serial number out of eeprom.
+*/
+void cal_load_sernum( system_state_t *sys_state ) {
+  uint8_t lowbyte = eeprom_read_char(SERNUM_ADDR);
+  uint8_t highbyte = eeprom_read_char(SERNUM_ADDR + 1);
+  sys_state -> sernum = (uint16_t)( (highbyte << 8) + lowbyte );
+  logger_msg_p( "cal", log_level_INFO,
+		PSTR("Writing serial number of %u\r\n"),
+		sys_state -> sernum);
+}
+
+/* Function called by the remote comand $setser
+ 
+   Sets the instrument's serial number.
+*/
+void cmd_write_sernum( command_arg_t *command_arg_ptr ) {
+  uint16_t sernum = (command_arg_ptr -> uint16_arg);
+  cal_save_sernum(sernum);
+  functions_init();
 }
